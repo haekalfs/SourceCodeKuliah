800b4bbbb55ddd0ed36d47dc31fe911aff5c4311
haekal <haekal@perdana.co.id>
2021-11-21 19:11:15 +0700

    Edited jam 7

--- codeExplanation.py
+++ codeExplanation.py
@@ -2,10 +2,13 @@ print("Soal Matriks Logika & Algoritma")
 Enter = input("Tekan Enter Untuk Melanjutkan...")
 print("Contoh Soal :")
 
+## Kode Def dibawah ini fungsinya adalah untuk mencetak list angka [[1,2,3],[1,2,3],[1,2,3]] menjadi bentuk matriks ordo 3x3##
+#sehingga kita hanya perlu menulis fungsiPrint(matriks1) maka matriks 1 akan dicetak dalam bentuk ordo 3x3# 
 def fungsiPrint(valueDrPrint):
     for i in valueDrPrint:
         print(i)
 
+## Kode dibawah adalah matriks ordo 3x3
 matriks1 = [
     [1, 2, 3],
     [3, 4, 5],
@@ -25,6 +28,8 @@ print("Matriks 2 :")
 fungsiPrint(matriks1)
 print(" ")
 
+## Kode dibawah adalah kode fungsi untuk menghitung penjumlahan matriks yg dapat dipanggil kapan saja
+# Sehingga kita hanya perlu menulis hitungMatriks(matriks1, matriks2)
 def hitungMatriks(valMatriks1, valMatriks2):
     for i in range(0, len(valMatriks1)):
         kolomMatriks = []
@@ -43,13 +48,11 @@ if (User=="Y" or User=="y"):
     def codeOwner(nama):
         print("Tugas Kelompok =", nama)
 
-    def fungsiPrint(valueDrPrint):
-        for i in valueDrPrint:
-            print(i)
-
+    ##Kode dibawah adalah mylist
     Matriks1 = []
     Matriks2 = []
 
+    ##kode dibawah adalah input an untuk matriks1 dimana dibuat untuk menambahkan value yang diinput user ke dalam mylist##
     a, b, c = [int(x) for x in input("Input 3 Angka Untuk Matriks 1 Baris ke-1: Format 1,2,3\n").split(',' or ', ' or ' ,' or ' , ')]
     valMatriks = [a, b, c]
     Matriks1.append(valMatriks)
@@ -65,7 +68,8 @@ if (User=="Y" or User=="y"):
     print(" ")
     print("\r+\r")
     print(" ")
-
+    
+    ##kode dibawah adalah input an untuk matriks2 dimana dibuat untuk menambahkan value yang diinput user ke dalam mylist##
     a, b, c = [int(x) for x in input("Input 3 Angka Untuk Matriks 2 Baris 1:\n").split(',' or ', ' or ' ,' or ' , ')]
     valMatriks = [a, b, c]
     Matriks2.append(valMatriks)
@@ -82,21 +86,14 @@ if (User=="Y" or User=="y"):
     print("Loading Data... Please Wait...")
     print(" ")
     print("Nilai Matriks 1 mu :")
-    fungsiPrint(Matriks1)
+    fungsiPrint(Matriks1)## Terakhir kita hanya perlu mencetak/print matriks-matriksnya dengan fungsi def fungsiPrint tadi
     print(" ")
     print("Nilai Matriks 2 mu :")
-    fungsiPrint(Matriks2)
+    fungsiPrint(Matriks2)## Terakhir kita hanya perlu mencetak/print matriks-matriksnya dengan fungsi def fungsiPrint tadi
     print(" ")
-
-    def hitungMatriks(valMatriks1, valMatriks2):
-        for i in range(0, len(valMatriks1)):
-            kolomMatriks = []
-            for j in range(0, len(valMatriks1[0])):
-                kolomMatriks.append(valMatriks1[i][j] + valMatriks2[i][j])
-            print(kolomMatriks)
 
     print("Hasil dari Penjumlahan Matriks mu yaitu :")
-    hitungMatriks(Matriks1, Matriks2)
+    hitungMatriks(Matriks1, Matriks2)## Terakhir kita hanya perlu mencetak/print matriks-matriksnya dengan fungsi def hitungMatriks tadi
 
     print(" ")
-    codeOwner("Haekal Sastradilaga : 19210677 \nNadif : 19, Fauzan : 19, Rifkhy : 19, Fariz : 19")
+    codeOwner("Haekal Sastradilaga : 19210677 \nNadif : 19210865, Fauzan : 19211304, Rifkhy : 19210944, Fariz : 19")
