1ef8f6f6b2a4ccdd82aa5e8645072a3378cbe756
haekal <haekal@perdana.co.id>
2021-11-29 15:20:13 +0700

    edited hari senin november 29

---
+++ eBPM/classCreditseBPM.py
@@ -0,0 +1,20 @@
+from prettytable import PrettyTable
+
+tblBrg = PrettyTable()
+
+print("-------------------------------------------------------------------")
+print("              Thank you for using our program (eBPM)")
+print("-------------------------------------------------------------------")
+
+enter = input("Tekan enter untuk melanjutkan...\n")
+
+print("""List Contributors :
+Haekal Sastradilaga : Lead
+Fariz Darmawan : Member
+Rifkhy Eka : Member
+Fauzan : Member
+Nadif : Member """)
+
+
+
+""" Copyright to Haekal Sastradilaga"""
